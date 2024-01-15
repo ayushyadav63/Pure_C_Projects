ac016089ec3fc48114d664d7077574daec9978ba
ayushyadav63 <155818170+ayushyadav63@users.noreply.github.com>
2024-01-16 00:07:08 +0530

    Add files via upload

---
+++ Matrix_Multiplier.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f,g,h,k,j,n,m;
+    printf("enter the first matrix row wise");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    printf("enter the second matrix column wise");
+    scanf("%d %d %d %d",&e,&f,&g,&h);
+    
+    k=(a*e) + (b*f);
+    j=(a*g) + (b*h);
+    n=(c*e) + (d*f);
+    m=(c*g) + (d*h);
+    
+    printf("matrix multiplication result");
+    printf("\n%d %d \n%d %d",k,j,n,m);
+    
+   
+
+    return 0;
+}
